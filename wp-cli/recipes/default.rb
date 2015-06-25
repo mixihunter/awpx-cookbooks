19fc6894326a93caed1681ad3497e84a74629cae
mixihunter <triplesky@gmail.com>
2015-06-25 17:59:31 +0900

    add

---
+++ wp-cli/recipes/default.rb
@@ -0,0 +1,9 @@
+wpdir = "/usr/bin/"
+
+#/usr/bin/直下にwordpressコマンドラインツールをダウンロードする
+remote_file "#{wpdir}/wp" do
+  source "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
+  owner "root"
+  group "root"
+  mode 00755
+end
